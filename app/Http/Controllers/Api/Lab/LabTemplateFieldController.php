fdf737f50334636e324117441bb90b225143ddee
OPIYO OSCAR <opiyooscar414@gmail.com>
2026-04-30 02:41:12 +0300

    Bug fix.

--- app/Http/Controllers/Api/Lab/LabTemplateFieldController.php
+++ app/Http/Controllers/Api/Lab/LabTemplateFieldController.php
@@ -518,7 +518,6 @@ class LabTemplateFieldController extends Controller
      */
     public function bulkUpdateOrders(Request $request): JsonResponse
     {
-        Log::info($request);
         try {
             $request->validate([
                 'orders' => 'required|array|min:1',
